56c89910cc27b4cfeeffb9495f1299a6c62f58ca
Brodino <brodino96@gmail.com>
2024-02-17 20:51:35 +0100

    Added anim when item is used

--- server/server.lua
+++ server/server.lua
@@ -1,8 +1,8 @@
 ESX = exports["es_extended"]:getSharedObject()
 
 for _, i in ipairs(Config.items) do
-    ESX.RegisterUsableItem(i.itemName, function ()
-        print("Ho usato l'item "..i.label)
+    ESX.RegisterUsableItem(i.itemName, function (source)
+        TriggerClientEvent("dpemote:playemote", source, "book")
     end)
 end
 
